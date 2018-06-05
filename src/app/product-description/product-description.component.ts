91c600c93f28f90557e14064f41bf179703ff824
Sébastien Cadorette <cado.94@me.com>
2018-06-05 09:26:48 -0400

    Module 3
    fixes

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -7,7 +7,7 @@ import { ProductService } from './../product.service';
   styleUrls: ['./product-description.component.css']
 })
 export class ProductDescriptionComponent implements OnInit {
-    private albumInfo;
+    albumInfo;
 
   constructor(private _productService: ProductService) { }
 
