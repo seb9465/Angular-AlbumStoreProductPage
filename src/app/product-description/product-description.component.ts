6c5c773b018f4129bac7e56f535f04cf2f3c24fd
Sébastien Cadorette <cado.94@me.com>
2018-06-05 09:19:35 -0400

    Module 3
    Subscribing to the get album function of the product service.

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -7,11 +7,12 @@ import { ProductService } from './../product.service';
   styleUrls: ['./product-description.component.css']
 })
 export class ProductDescriptionComponent implements OnInit {
-    private _albumInfo;
+    private albumInfo;
 
   constructor(private _productService: ProductService) { }
 
   ngOnInit() {
+      this._productService.getAlbum(1).subscribe( response => this.albumInfo = response );
   }
 
 }
