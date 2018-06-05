387abcb5fc9ae7f29fbdce394954c9aa29d6a572
Sébastien Cadorette <cado.94@me.com>
2018-06-05 09:16:34 -0400

    Module 2
    fixes

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from './../product.service';
 
 @Component({
   selector: 'app-product-description',
@@ -6,8 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-description.component.css']
 })
 export class ProductDescriptionComponent implements OnInit {
+    private _albumInfo;
 
-  constructor() { }
+  constructor(private _productService: ProductService) { }
 
   ngOnInit() {
   }
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -4,13 +4,13 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
-    private _albumUrl = '../assets/album.json'
+    private _albumUrl = '../assets/album.json';
 
     constructor(private _http: Http) { }
 
-    public getAlbum(id: number) {
-        return this._http.get(this._albumUrl).map((response) => {
-            response.json();
-        });
+    getAlbum(id: number) {
+        return this._http.get(this._albumUrl).map((response) =>
+            response.json()
+        );
     }
 }
